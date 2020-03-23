aa0be311aeaaf68fc88dfe32284aec0c9b28fc02
huangzb <1413213815@qq.com>
2020-03-23 18:39:47 +0800

    实现图片的base64解析成图片，现在遇到图片的无法进行矩阵的乘法问题

--- PHashDll/mian.cpp
+++ PHashDll/mian.cpp
@@ -5,41 +5,94 @@
 #include <opencv2\opencv.hpp>
 #include <opencv2\core\core.hpp>
 #include <opencv2\core\mat.hpp>
+#include <opencv2\highgui\highgui.hpp>
+#include "opencv2/imgcodecs/legacy/constants_c.h"
+#include "func.cpp"
+
 using namespace std;
 using namespace cv;
+
 int fingerprint(Mat src, Mat* hash);
 String init(Mat src);
+
 JNIEXPORT jstring JNICALL Java_com_huangzb_demo_PHash_PHashMatch
 (JNIEnv* env, jobject obj, jstring srcStr) {
- char const* str;
-str = env->GetStringUTFChars(srcStr, 0);
-if(str == NULL) {
-return NULL; 
+	char const* str;
+	str = env->GetStringUTFChars(srcStr, 0);
+	if (str == NULL) {
+		return NULL;
+	}
+
+	//std::cout << str << std::endl;
+	//Mat src = imread("F:\\testpic\\chun.jpg", 0);
+	//String outputpath = init(src);
+	Mat src = imread(str, 0);
+	String outputpath = init(src);
+	//释放资源
+	env->ReleaseStringUTFChars(srcStr, str);
+
+	// 返回一个字符串
+	char const* tmpstr = outputpath.c_str();
+	jstring rtstr = env->NewStringUTF(tmpstr);
+	return rtstr;
 }
 
-std::cout << str << std::endl;
-//Mat src = imread("F:\\testpic\\chun.jpg", 0);
-//String outputpath = init(src);
-Mat src = imread(str, 0);
-String outputpath = init(src);
-//释放资源
-env->ReleaseStringUTFChars(srcStr, str);
-
-// 返回一个字符串
-char const* tmpstr = outputpath.c_str();
-jstring rtstr = env->NewStringUTF(tmpstr);
-return rtstr;
-}
 JNIEXPORT jstring JNICALL Java_com_huangzb_demo_PHash_PHashMatchMore
 (JNIEnv* env, jobject obj, jobject jobj1, jobject jobj2) {
+	//获得pic类引用
+	jclass pic_cla = env->GetObjectClass(jobj1);
+	if (pic_cla == NULL)
+	{
+		cout << "GetObjectClass failed \n";
+	}
+	//获取类中的数据
+	jfieldID idFieldID = env->GetFieldID(pic_cla, "id", "Ljava/lang/String;"); //获得得Student类的属性id 
+	jfieldID picdataFieldID = env->GetFieldID(pic_cla, "picdata", "Ljava/lang/String;"); // 获得属性ID
 
+	//将类中的数据转换成string
+	//1.jstring接收属性值
+	jstring id = (jstring)env->GetObjectField(jobj1, idFieldID);  //获得属性值
+	jstring picdata = (jstring)env->GetObjectField(jobj1, picdataFieldID);//获得属性值
+	//2.const char*接收转换的jstring
+	const char* c_picdata = env->GetStringUTFChars(picdata, NULL);//转换成 char *
+	const char* c_id = env->GetStringUTFChars(id, NULL);//转换成 char *
+	//3.const char*转换成string类型
+	string str_picdata = c_picdata;
+	string str_id = c_id;
+	//4.释放引用
+	env->ReleaseStringUTFChars(picdata, c_picdata);
+	env->ReleaseStringUTFChars(id, c_id);
+
+	cout << "=======C++====" << endl;
+	///*cout << " at Native age is :" << id << " # name is " << str_name << endl;*/
+	///*const char* str = "C++String";*/
+	//jstring rtstr = env->NewStringUTF(str);//env->NewStringUTF(c_name);
+
+	//将传送来的字符串转换成mat图像方便进行图像处理
+	Mat mymat = Base2Mat(str_picdata);
+
+	//将mat图像保存在本地
+	imwrite("F:\\PHashPic1.jpg", mymat);
+
+	String outputpath = init(mymat);
+
+	// 返回一个字符串
+	char const* tmpstr = outputpath.c_str();
+	jstring rtstr = env->NewStringUTF(tmpstr);
+	return rtstr;
 }
 int fingerprint(Mat src, Mat* hash)
 {
+	cout << "Mat  src.type()=" << src.type() << "\n";
 	resize(src, src, Size(32, 32));
-	src.convertTo(src, CV_32F);
+	src.convertTo(src, CV_32FC1, 1.0/255.0);
+	/*src.convertTo(src, CV_32F);*/
+	cout << "src.type()="<<src.type() << "\n";
 	Mat srcDCT;
+	cout <<"src.type()"<< src.type() << endl;
+	cout << "srcDCT.type()" << srcDCT.type() << endl;
 	dct(src, srcDCT);
+	cout << "dct(src, srcDCT);"  << "\n";
 	srcDCT = abs(srcDCT);
 	double sum = 0;
 	for (int i = 0; i < 8; i++)
--- PHashDll/pch.h
+++ PHashDll/pch.h
@@ -3,11 +3,15 @@
 // 这还将影响 IntelliSense 性能，包括代码完成和许多代码浏览功能。
 // 但是，如果此处列出的文件中的任何一个在生成之间有更新，它们全部都将被重新编译。
 // 请勿在此处添加要频繁更新的文件，这将使得性能优势无效。
-
-#ifndef PCH_H
 #define PCH_H
 
 // 添加要在此处预编译的标头
 #include "framework.h"
+#ifndef BASE64_H_C0CE2A47_D10E_42C9_A27C_C883944E704A
+#define BASE64_H_C0CE2A47_D10E_42C9_A27C_C883944E704A
 
+#include <string>
+
+std::string base64_encode(unsigned char const*, unsigned int len);
+std::string base64_decode(std::string const& s);
 #endif //PCH_H
