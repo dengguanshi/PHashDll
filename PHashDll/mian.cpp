63da361ed5e1077c2b2cf52a8c3c9dbc2cd99afb
huangzb <1413213815@qq.com>
2020-03-24 00:22:45 +0800

    测试实例完成

--- PHashDll/mian.cpp
+++ PHashDll/mian.cpp
@@ -23,6 +23,9 @@ public:
 int fingerprint(Mat src, Mat* hash);
 String init(Mat src);
 Mat MyJobject2Mat(JNIEnv* env, jobject jobj1);
+Pic MyJobject2Mat2(JNIEnv* env, jobject jobj1);
+String  init2(Mat src, Pic PicArray[]);
+
 JNIEXPORT jstring JNICALL Java_com_huangzb_demo_PHash_PHashMatch
 (JNIEnv* env, jobject obj, jstring srcStr) {
 	char const* str;
@@ -55,19 +58,60 @@ JNIEXPORT jstring JNICALL Java_com_huangzb_demo_PHash_PHashMatchMore
 	//获取长度
 	jint len = env->CallIntMethod(jobjList, arraylist_size);
 	Pic PicArray[100];
+	cout << "len"<<len << endl;
 	for (int i = 0; i < len; i++) {
 		jobject obj_user = env->CallObjectMethod(jobjList, arraylist_get, i);
-
-
+		PicArray[i] = MyJobject2Mat2(env, obj_user);
+		cout << "=PicArray[i]=="<< PicArray[i] .id<< endl;
 	}
 
-	String outputpath = init(MyJobject2Mat(env,jobj1));
+	String outputpath = init2(MyJobject2Mat2(env,jobj1).picMat, PicArray);
 	// 返回一个字符串
 	char const* tmpstr = outputpath.c_str();
 	jstring rtstr = env->NewStringUTF(tmpstr);
 	return rtstr;
 }
 
+Pic MyJobject2Mat2(JNIEnv* env, jobject jobj1) {
+	//获得pic类引用
+	jclass pic_cla = env->GetObjectClass(jobj1);
+	if (pic_cla == NULL)
+	{
+		cout << "GetObjectClass failed \n";
+	}
+	//获取类中的数据
+	jfieldID idFieldID = env->GetFieldID(pic_cla, "id", "Ljava/lang/String;"); //获得得Student类的属性id 
+	jfieldID picdataFieldID = env->GetFieldID(pic_cla, "picdata", "Ljava/lang/String;"); // 获得属性ID
+
+	//将类中的数据转换成string
+	//1.jstring接收属性值
+	jstring id = (jstring)env->GetObjectField(jobj1, idFieldID);  //获得属性值
+	jstring picdata = (jstring)env->GetObjectField(jobj1, picdataFieldID);//获得属性值
+	//2.const char*接收转换的jstring
+	const char* c_picdata = env->GetStringUTFChars(picdata, NULL);//转换成 char *
+	const char* c_id = env->GetStringUTFChars(id, NULL);//转换成 char *
+	//3.const char*转换成string类型
+	string str_picdata = c_picdata;
+	string str_id = c_id;
+	//4.释放引用
+	env->ReleaseStringUTFChars(picdata, c_picdata);
+	env->ReleaseStringUTFChars(id, c_id);
+
+	cout << "=======C++====" << endl;
+	///*cout << " at Native age is :" << id << " # name is " << str_name << endl;*/
+	///*const char* str = "C++String";*/
+	//jstring rtstr = env->NewStringUTF(str);//env->NewStringUTF(c_name);
+
+	//将传送来的字符串转换成mat图像方便进行图像处理
+	Mat mymat = Base2Mat(str_picdata);
+	Mat testpic;
+	//将使用base64转换的图像转换成灰度图处理
+	cvtColor(mymat, testpic, CV_RGB2GRAY);
+	Pic outputPic;
+	outputPic.picMat = testpic;
+	outputPic.id = str_id;
+	return outputPic;
+}
 Mat MyJobject2Mat(JNIEnv* env, jobject jobj1) {
 	//获得pic类引用
 	jclass pic_cla = env->GetObjectClass(jobj1);
@@ -176,4 +220,42 @@ String  init(Mat src) {
 	String mypath = "F:\\testpic\\chun" + number + ".jpg";
 	cout << mypath << endl;
 	return mypath;
+}
+
+
+String  init2(Mat src, Pic PicArray[]) {
+
+	if (src.empty())
+	{
+		cout << "the image is not exist" << endl;
+		return "";
+	}
+	Mat srchash, dsthash;
+	fingerprint(src, &srchash);
+	int min = 100; string mini;
+	for (int i = 1; i <= 2; i++)
+	{
+		
+		Mat dst = PicArray[i-1].picMat;
+		if (dst.empty())
+		{
+			cout << "the image is not exist" << endl;
+			return "";
+		}
+		fingerprint(dst, &dsthash);
+		int d = 0;
+		for (int n = 0; n < srchash.size[1]; n++)
+			if (srchash.at<uchar>(0, n) != dsthash.at<uchar>(0, n)) d++;
+
+		cout << "chun" << i << " distance= " << d << "\n";
+		if (d < min) {
+			min = d;
+			mini = PicArray[i-1].id;
+		}
+		//personnel[i] = d;
+
+	}
+
+	cout << mini << endl;
+	return mini;
 }
